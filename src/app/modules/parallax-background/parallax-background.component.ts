5894700289208020935f43f1d14cf3d1ed20f6f2
David Espinoza <deerhou@gmail.com>
2024-02-12 12:00:56 -0500

    agragando imagenes para el escenario

--- src/app/modules/parallax-background/parallax-background.component.ts
+++ src/app/modules/parallax-background/parallax-background.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-parallax-background',
   standalone: true,
   imports: [],
   templateUrl: './parallax-background.component.html',
-  styleUrl: './parallax-background.component.scss'
+  styleUrl: './parallax-background.component.scss',
 })
-export class ParallaxBackgroundComponent {
+export class ParallaxBackgroundComponent implements OnInit {
+  @ViewChild('canvas1', { static: true })
+  canvas1!: ElementRef<HTMLCanvasElement>;
+  canvas!: HTMLCanvasElement;
+  ctx!: CanvasRenderingContext2D | null;
+  CANVAS_WIDTH: number = 800;
+  CANVAS_HEIGHT: number = 700;
+  gameSpeed = 5;
 
+  ngOnInit(): void {
+    try {
+      this.canvas = this.canvas1.nativeElement;
+      this.ctx = this.canvas.getContext('2d');
+      this.CANVAS_WIDTH = this.canvas.width = 800;
+      this.CANVAS_HEIGHT = this.canvas.height = 700;
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
